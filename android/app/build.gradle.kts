3589a30270d6ebcc84d8d631051830a90d012a75
mako <a20210646@pucp.edu.pe>
2026-07-02 14:39:40 -0500

    Acceso a internet

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "app.skybnb_app"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.6"
+        versionCode = 7
+        versionName = "1.0.7"
     }
 
     // Configuración de firma
