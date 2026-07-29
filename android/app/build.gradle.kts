1a3ee7e76109b7850d540eca4fa4b6844e415aba
mako <a20210646@pucp.edu.pe>
2026-07-29 08:53:21 -0500

    Update version 11

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "app.skybnb_app"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.0.10"
+        versionCode = 11
+        versionName = "1.0.11"
     }
 
     // Configuración de firma
